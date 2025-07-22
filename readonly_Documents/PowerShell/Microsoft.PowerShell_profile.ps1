e36600a52007e79dbfd0c23ec1e239e00f0e808d
Dukyoon Im <DUKY8N@gmail.com>
2025-07-23 03:10:58 +0900

    feat: add zoxide init and vim alias in PowerShell profile

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,6 @@
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
+
+Set-Alias vim nvim
+
 function pli { winget import -i C:\Users\duky8n\.local\share\chezmoi\.wingetfile.json }
 function ple { nvim C:\Users\duky8n\.local\share\chezmoi\.wingetfile.json }
