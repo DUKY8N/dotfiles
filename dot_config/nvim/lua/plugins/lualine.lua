02e13661b15fe3535e720099f5624d81bf906fbf
Dukyoon Im <DUKY8N@gmail.com>
2025-12-02 22:26:03 +0900

    feat(nvim): customize lualine layout with separators

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -1,5 +1,29 @@
 return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    opts = {},
+    opts = {
+        options = {
+            theme = 'auto',
+            component_separators = { left = '', right = '' },
+            section_separators = { left = '', right = '' },
+        },
+        sections = {
+            lualine_a = {
+                {
+                    'mode',
+                    separator = { left = '', right = '' },
+                },
+            },
+            lualine_b = { 'branch', 'diff', 'diagnostics' },
+            lualine_c = {},
+            lualine_x = { 'progress', 'location' },
+            lualine_y = {},
+            lualine_z = {
+                {
+                    'filename',
+                    separator = { left = '', right = '' },
+                },
+            },
+        },
+    },
 }
