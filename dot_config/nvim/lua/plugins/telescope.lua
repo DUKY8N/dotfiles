e9e90926179fdaaab0d56ab408fad3ee842cebfe
Dukyoon Im <DUKY8N@gmail.com>
2025-12-06 20:07:02 +0900

    fix(nvim): lazy-load telescope actions to avoid startup error

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -1,30 +1,31 @@
-local actions = require 'telescope.actions'
-
 return {
     'nvim-telescope/telescope.nvim',
     cmd = 'Telescope',
     dependencies = { 'nvim-lua/plenary.nvim' },
     keys = {
         { '<leader><leader>', '<cmd>Telescope find_files<cr>', desc = 'Find files' },
-        { '<leader>fb', '<cmd>Telescope buffers<cr>', desc = 'Buffers' },
-        { '<leader>ff', '<cmd>Telescope find_files<cr>', desc = 'Find files' },
-        { '<leader>fg', '<cmd>Telescope live_grep<cr>', desc = 'Live grep' },
-        { '<leader>fh', '<cmd>Telescope help_tags<cr>', desc = 'Help tags' },
+        { '<leader>fb',       '<cmd>Telescope buffers<cr>',    desc = 'Buffers' },
+        { '<leader>ff',       '<cmd>Telescope find_files<cr>', desc = 'Find files' },
+        { '<leader>fg',       '<cmd>Telescope live_grep<cr>',  desc = 'Live grep' },
+        { '<leader>fh',       '<cmd>Telescope help_tags<cr>',  desc = 'Help tags' },
     },
-    opts = {
-        defaults = {
-            mappings = {
-                i = {
-                    ['<esc>'] = actions.close,
-                    ['<C-j>'] = actions.move_selection_next,
-                    ['<C-k>'] = actions.move_selection_previous,
-                },
-                n = {
-                    ['<esc>'] = actions.close,
-                    ['<C-j>'] = actions.move_selection_next,
-                    ['<C-k>'] = actions.move_selection_previous,
+    opts = function()
+        local actions = require 'telescope.actions'
+        return {
+            defaults = {
+                mappings = {
+                    i = {
+                        ['<esc>'] = actions.close,
+                        ['<C-j>'] = actions.move_selection_next,
+                        ['<C-k>'] = actions.move_selection_previous,
+                    },
+                    n = {
+                        ['<esc>'] = actions.close,
+                        ['<C-j>'] = actions.move_selection_next,
+                        ['<C-k>'] = actions.move_selection_previous,
+                    },
                 },
             },
-        },
-    },
+        }
+    end,
 }
