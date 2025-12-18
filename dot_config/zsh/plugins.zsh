7aa4b489dc9d2d73943896abf77d3bb985283a4e
Dukyoon Im <DUKY8N@gmail.com>
2025-12-18 23:09:24 +0900

    chore(zsh): move plugins/tools/aliases into XDG config layout

---
+++ dot_config/zsh/plugins.zsh
@@ -0,0 +1,4 @@
+# zsh plugins
+[ -r "$HOME/.local/share/zsh/plugins/fzf-tab/fzf-tab.plugin.zsh" ] && source "$HOME/.local/share/zsh/plugins/fzf-tab/fzf-tab.plugin.zsh"
+[ -r "$HOME/.local/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" ] && source "$HOME/.local/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh"
+[ -r "$HOME/.local/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ] && source "$HOME/.local/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
---
+++ dot_config/zsh/tools.zsh
@@ -0,0 +1,21 @@
+# vivid (themes)
+export LS_COLORS="$(vivid generate catppuccin-mocha)"
+
+# eza
+export EZA_CONFIG_DIR="$HOME/.config/eza"
+
+# nvim
+export EDITOR="nvim"
+
+# man
+export MANPAGER="nvim +Man!"
+
+# mise
+eval "$(mise activate zsh)"
+# eval "$(mise completion zsh)"
+
+# zoxide
+eval "$(zoxide init zsh)"
+
+# starship
+eval "$(starship init zsh)"
