a1b222b0137152b205ac6b6a0dd5539cb8a0cf11
Dukyoon Im <DUKY8N@gmail.com>
2026-02-07 19:50:31 +0900

    feat(powershell): add yazi wrapper to cd into last dir

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -4,3 +4,13 @@ Set-Alias vim nvim
 
 function pli { winget import -i C:\Users\duky8n\.local\share\chezmoi\.wingetfile.json }
 function ple { nvim C:\Users\duky8n\.local\share\chezmoi\.wingetfile.json }
+
+function y {
+	$tmp = (New-TemporaryFile).FullName
+	yazi.exe $args --cwd-file="$tmp"
+	$cwd = Get-Content -Path $tmp -Encoding UTF8
+	if ($cwd -ne $PWD.Path -and (Test-Path -LiteralPath $cwd -PathType Container)) {
+		Set-Location -LiteralPath (Resolve-Path -LiteralPath $cwd).Path
+	}
+	Remove-Item -Path $tmp
+}
