a1e5fcc4f68ac4193a67d0a452cc10fea15648f3
Dukyoon Im <DUKY8N@gmail.com>
2026-05-18 16:46:29 +0900

    feat(nvim): close Snacks picker with Escape

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -1,7 +1,14 @@
 return {
     bigfile = { enabled = true },
-    indent = { enabled = true, },
-    picker = { enabled = true, },
-    quickfile = { enabled = true, },
-    scope = { enabled = true, },
+    indent = { enabled = true },
+    quickfile = { enabled = true },
+    scope = { enabled = true },
+
+    picker = {
+        enabled = true,
+        win = {
+            input = { keys = { ['<Esc>'] = { 'close', mode = { 'n', 'i' } } } },
+            list = { keys = { ['<Esc>'] = { 'close', mode = { 'n', 'i' } } } },
+        },
+    },
 }
